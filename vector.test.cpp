3b502f88e405437af9e0de765fdd293978bc01dd
Federico Mastroforti <federico.mastroforti@studio.unibo.it>
2023-06-28 14:20:39 +0200

    added vector.test.cpp

---
+++ vector.test.cpp
@@ -0,0 +1,45 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "vector.hpp"
+
+#include "body.hpp"
+#include "doctest.h"
+
+TEST_CASE("testing Vector class") {
+  SUBCASE("Testing norm function") {
+    Vector a{1, 0};
+    Vector b{0, 1};
+    Vector c{0, 0};
+    Vector d{3, 4};
+    Vector e{-3,-4};
+    CHECK(a.norm() == 1);
+    CHECK(b.norm() == 1);
+    CHECK(c.norm() == 0);
+    CHECK(d.norm() == 5);
+    CHECK(e.norm()==5);
+  }
+
+  SUBCASE("Testing distance function"){
+    Vector a{0,0};
+    Vector b{0,1};
+    Vector c{1,0};
+    Vector d{1,1};
+    Vector e{0,-1};
+    Vector f{-1,0};
+    Vector g{-1,-1};
+    CHECK(a.distance(b)==1);
+    CHECK(b.distance(a)==1);
+    CHECK(c.distance(a)==1);
+    CHECK(a.distance(c)==11);
+    CHECK(d.distance(c)==1);
+    CHECK(c.distance(d)==1);
+    CHECK(b.distance(e)==2);
+    CHECK(f.distance(b)==doctest::Approx(1.414));
+    CHECK(g.distance(a)==doctest::Approx(1.414));
+    CHECK(a.distance(a)==0);
+    CHECK(e.distance(e)==0);
+  }
+
+  
+
+  ;
+}
