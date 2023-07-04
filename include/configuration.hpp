8ce190e58fde178fa666eb7bd454da28a890404a
Nico Tortorici <tortoricinico@outlook.it>
2023-07-04 19:06:35 +0200

    Added many commands to body and configuration header files.

--- include/configuration.hpp
+++ include/configuration.hpp
@@ -2,13 +2,12 @@
 #define CONFIGURATION_H
 
 #include <SFML/Graphics.hpp>
-#include <iostream>
 #include <vector>
 
+#include "SFML/Graphics.hpp"
 #include "body.hpp"
 #include "physics_engine.hpp"
 #include "renderer.hpp"
-#include "SFML/Graphics.hpp"
 
 class Configuration {
  private:
@@ -19,15 +18,38 @@ class Configuration {
   sf::RenderWindow& _window;
 
  public:
-  Configuration(std::string name, double universeWidth, double timeScale, sf::RenderWindow& window);
+  Configuration(std::string name, double universeWidth, double timeScale,
+                sf::RenderWindow& window);
 
+  /**
+   * Adds a body the the configuration.
+   * 
+   * @param body The body being added to the configuration.
+  */
   void addBody(std::unique_ptr<Body>& body);
 
+  /**
+   * Creates the Renderer, used to draw the objects on the screen.
+   * 
+   * @return The pointer to the created renderer.
+  */
   std::unique_ptr<Renderer> createRenderer() const;
+
+  /**
+   * Creates the PhysicsEngine object, used in the simulation to apply forces and calculate body's position in each.
+   * 
+   * @return The pointer to the created PhysicsEngine.
+  */
   std::unique_ptr<PhysicsEngine> createPhysicsEngine() const;
 
+  /**
+   * Creates the bodies according to the configuration.
+   * 
+   * @return A vector containing pointers to the bodies.
+  */
   std::vector<std::unique_ptr<Body>> createBodies() const;
 
+  // Configuration's fields make it non-copyable. 
   Configuration(const Configuration&) = delete;
   Configuration& operator=(Configuration&) = delete;
 };
