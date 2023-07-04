6323081bf718811b7cbaa75dae6a708cf22bcd18
Federico Mastroforti <federico.mastroforti@studio.unibo.it>
2023-07-04 12:05:19 +0200

    again tests

--- test/configurations.test.cpp
+++ test/configurations.test.cpp
@@ -11,11 +11,25 @@ TEST_CASE("Testing getConfigurations function"){
     std::vector<std::shared_ptr<Configuration>> a= getConfigurations(window);
     std::shared_ptr<Configuration> b= a[0];
     std::vector<std::unique_ptr<Body>> bodies=b->createBodies();
+    auto height = 2*MARS_AFELIO  + EARTH_SUN_DISTANCE;
     
     CHECK(bodies[0]->getMass()==SUN_MASS);
     CHECK(bodies[1]->getMass()==EARTH_MASS);
-    CHECK(bodies[2]->getMass()==0.55*EARTH_MASS);
-    CHECK(bodies[3]->getMass()==0.815*EARTH_MASS);
-    CHECK(bodies[4]->getMass()==0.1*EARTH_MASS);
+    CHECK(bodies[2]->getMass()==MERCURY_MASS);
+    CHECK(bodies[3]->getMass()==VENUS_MASS);
+    CHECK(bodies[4]->getMass()==MARS_MASS);
     CHECK(bodies[5]->getMass()==MOON_MASS);
+
+    CHECK(bodies[0]->getPosition()==Vector{0.5 * 1E5 + MARS_AFELIO + MARS_AFELIO / 2,
+             height / 2});
+    CHECK(bodies[1]->getPosition()==Vector{0.5 * 1E5 + MARS_AFELIO + MARS_AFELIO / 2+EARTH_SUN_DISTANCE,
+             height / 2});
+    CHECK(bodies[2]->getPosition()==Vector{0.5 * 1E5 + MARS_AFELIO + MARS_AFELIO / 2- MERCURY_AFELIO,
+             height / 2});
+    CHECK(bodies[3]->getPosition()==Vector{0.5 * 1E5 + MARS_AFELIO + MARS_AFELIO / 2- VENUS_AFELIO,
+             height / 2});
+    CHECK(bodies[4]->getPosition()==Vector{});
+    CHECK(bodies[5]->getPosition()==Vector{});
+
+
 }
