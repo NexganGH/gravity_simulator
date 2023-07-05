354e8a03b587e701e97f090ee0edbbe4c2f4ee91
Federico Mastroforti <federico.mastroforti@studio.unibo.it>
2023-07-05 12:39:09 +0200

    renderer test setup

---
+++ test/renderer.test.cpp
@@ -0,0 +1,21 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "simulation_state.hpp"
+
+#include <SFML/Graphics.hpp>
+
+#include "body.hpp"
+#include "configurations.hpp"
+#include "doctest.h"
+
+TEST_CASE("Testing Renderer class"){
+SUBCASE("Tetsing "){}
+
+
+
+
+
+
+
+
+}
