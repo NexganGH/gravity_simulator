e4485b6abf4bc1fd8e64b152ce154875acf5a358
Federico Mastroforti <federico.mastroforti@studio.unibo.it>
2023-07-06 22:09:11 +0200

    changes on tests, added renderer.test.cpp

--- test/renderer.test.cpp
+++ test/renderer.test.cpp
@@ -9,13 +9,28 @@
 #include "doctest.h"
 
 TEST_CASE("Testing Renderer class"){
-SUBCASE("Tetsing "){}
-
-
-
-
-
-
-
-
+    sf::RenderWindow window;
+    gs::Renderer renderer(window, 1);
+SUBCASE("Tetsing screenToReal class"){
+    gs::Vector a{1,1};
+    gs::Vector b{0,0};
+    gs::Vector c{-1,-1};
+    gs::Vector d=renderer.screenToReal(a);
+    gs::Vector e=renderer.screenToReal(b);
+    gs::Vector f=renderer.screenToReal(c);
+    CHECK(a==d);
+    CHECK(b==e);
+    CHECK(c==f);
+}
+SUBCASE("Testing realToScreen"){
+    sf::Vector2f a{1,1};
+    sf::Vector2f b{0,0};
+    sf::Vector2f c{-1,-1};
+    sf::Vector2f d=renderer.realToScreen(a);
+    sf::Vector2f e=renderer.realToScreen(b);
+    sf::Vector2f f=renderer.realToScreen(c);
+    CHECK(a==d);
+    CHECK(b==e);
+    CHECK(c==f);
+}
 }
