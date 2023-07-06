b69d38dc2615f8dd0bebad0a5727185118ae5ded
Gioele Mancino <mancinogioele6@gmail.com>
2023-07-06 09:49:26 +0200

    Started to improve configurations file

--- src/configurations.cpp
+++ src/configurations.cpp
@@ -4,79 +4,72 @@
 #include <vector>
 
 #include "SFML/Graphics.hpp"
-#include "SFML/Graphics.hpp"
 #include "body.hpp"
 #include "configuration.hpp"
 #include "renderer.hpp"
+namespace gs {
 
 std::vector<std::shared_ptr<Configuration>> getConfigurations(
     sf::RenderWindow& window) {
   std::vector<std::shared_ptr<Configuration>> list;
 
-  CreateSolarSystem(list, window);
-  CreateEarthAndSun(list, window);
-  CreateStarCollision(list, window);
-  CreateStableOrbitTwoPlanets(list, window);
-  CreateStableOrbitTwoPlanets2(list, window);
-  CreateThreeBodies(list, window);
-  CreateBinaryStars(list, window);
-  CreateCollapsingBinaryStars(list, window);
+  list.push_back(CreateSolarSystem(window));
+  list.push_back(CreateEarthAndSun(window));
+  list.push_back(CreateThreeBodies(window));
 
   return list;
 }
 
-void CreateSolarSystem(std::vector<std::shared_ptr<Configuration>> list,
-                       sf::RenderWindow& window) {
+auto CreateSolarSystem(sf::RenderWindow& window) {
+  const float EARTH_SUN_DISTANCE = 149597870700;
+  const float EARTH_SPEED = 30.24E3;
+  const float EARTH_MASS = 5.9722E24;
+  const float SUN_MASS = 1.98855E30;
 
-const float EARTH_SUN_DISTANCE = 149597870700;
-const float EARTH_SPEED = 30.24E3;
-const float EARTH_MASS = 5.9722E24;
-const float SUN_MASS = 1.98855E30;
+  const float MERCURY_AFELIO = 69.8E9;
+  const float MERCURY_AFELIO_SPEED = 39.0E3;
+  const float MERCURY_MASS = 0.055 * EARTH_MASS;
+  const float MERCURY_AFELIO = 69.8E9;
+  const float MERCURY_AFELIO_SPEED = 39.0E3;
+  const float MERCURY_MASS = 0.055 * EARTH_MASS;
 
-const float MERCURY_AFELIO = 69.8E9;
-const float MERCURY_AFELIO_SPEED = 39.0E3;
-const float MERCURY_MASS = 0.055 * EARTH_MASS;
-const float MERCURY_AFELIO = 69.8E9;
-const float MERCURY_AFELIO_SPEED = 39.0E3;
-const float MERCURY_MASS = 0.055 * EARTH_MASS;
+  const float MOON_AFELIO = 405500E3;
+  const float MOON_AFELIO_SPEED = 0.970E3;
+  const float MOON_MASS = 7.3476E22;
+  const float MOON_AFELIO = 405500E3;
+  const float MOON_AFELIO_SPEED = 0.970E3;
+  const float MOON_MASS = 7.3476E22;
 
-const float MOON_AFELIO = 405500E3;
-const float MOON_AFELIO_SPEED = 0.970E3;
-const float MOON_MASS = 7.3476E22;
-const float MOON_AFELIO = 405500E3;
-const float MOON_AFELIO_SPEED = 0.970E3;
-const float MOON_MASS = 7.3476E22;
+  const float VENUS_AFELIO = 109.8E9;
+  const float VENUS_AFELIO_SPEED = 34.78E3;
+  const float VENUS_MASS = 0.815 * EARTH_MASS;
+  const float VENUS_AFELIO = 109.8E9;
+  const float VENUS_AFELIO_SPEED = 34.78E3;
+  const float VENUS_MASS = 0.815 * EARTH_MASS;
 
-const float VENUS_AFELIO = 109.8E9;
-const float VENUS_AFELIO_SPEED = 34.78E3;
-const float VENUS_MASS = 0.815 * EARTH_MASS;
-const float VENUS_AFELIO = 109.8E9;
-const float VENUS_AFELIO_SPEED = 34.78E3;
-const float VENUS_MASS = 0.815 * EARTH_MASS;
+  const float MARS_AFELIO = 249.261E9;
+  const float MARS_AFELIO_SPEED = 21.97E3;
+  const float MARS_MASS = 0.1 * EARTH_MASS;
+  const float MARS_AFELIO = 249.261E9;
+  const float MARS_AFELIO_SPEED = 21.97E3;
+  const float MARS_MASS = 0.1 * EARTH_MASS;
 
-const float MARS_AFELIO = 249.261E9;
-const float MARS_AFELIO_SPEED = 21.97E3;
-const float MARS_MASS = 0.1 * EARTH_MASS;
-const float MARS_AFELIO = 249.261E9;
-const float MARS_AFELIO_SPEED = 21.97E3;
-const float MARS_MASS = 0.1 * EARTH_MASS;
+  const float JUPITER_AFELIO = 816.363E9;
+  const float JUPITER_AFELIO_SPEED = 12.44E3;
+  const float JUPITER_MASS = 318 * EARTH_MASS;
+  const float JUPITER_AFELIO = 816.363E9;
+  const float JUPITER_AFELIO_SPEED = 12.44E3;
+  const float JUPITER_MASS = 318 * EARTH_MASS;
 
-const float JUPITER_AFELIO = 816.363E9;
-const float JUPITER_AFELIO_SPEED = 12.44E3;
-const float JUPITER_MASS = 318 * EARTH_MASS;
-const float JUPITER_AFELIO = 816.363E9;
-const float JUPITER_AFELIO_SPEED = 12.44E3;
-const float JUPITER_MASS = 318 * EARTH_MASS;
+  const float NEPTUNE_AFELIO = 30.34 * EARTH_SUN_DISTANCE;
+  const float NEPTUNE_AFELIO_SPEED = 5.37E3;
+  const float NEPTUNE_MASS = 17 * EARTH_MASS;
+  const float NEPTUNE_AFELIO = 30.34 * EARTH_SUN_DISTANCE;
+  const float NEPTUNE_AFELIO_SPEED = 5.37E3;
+  const float NEPTUNE_MASS = 17 * EARTH_MASS;
 
-const float NEPTUNE_AFELIO = 30.34 * EARTH_SUN_DISTANCE;
-const float NEPTUNE_AFELIO_SPEED = 5.37E3;
-const float NEPTUNE_MASS = 17 * EARTH_MASS;
-const float NEPTUNE_AFELIO = 30.34 * EARTH_SUN_DISTANCE;
-const float NEPTUNE_AFELIO_SPEED = 5.37E3;
-const float NEPTUNE_MASS = 17 * EARTH_MASS;
-
-  auto SolarSystem =
-      new Configuration("Solar System", 3 * MARS_AFELIO, 1000000, window);
+  auto SolarSystem = std::make_shared<Configuration>(
+      "Solar System", 3 * MARS_AFELIO, 1000000, window);
 
   auto height = 3 * MARS_AFELIO;
 
@@ -115,18 +108,17 @@ const float NEPTUNE_MASS = 17 * EARTH_MASS;
   SolarSystem->addBody(moon);
   // SolarSystem->addBody(neptune);
 
-  list.push_back(std::unique_ptr<Configuration>(SolarSystem));
+  return SolarSystem;
 }
 
-void CreateEarthAndSun(std::vector<std::shared_ptr<Configuration>> list,
-                       sf::RenderWindow& window) {
+auto CreateEarthAndSun(sf::RenderWindow& window) {
   const float EARTH_SUN_DISTANCE = 149597870700;
   const float EARTH_SPEED = 30.24E3;
   const float EARTH_MASS = 5.9722E24;
   const float SUN_MASS = 1.98855E30;
 
-  auto EarthAndSun =
-      new Configuration("Earth & Sun", 3 * EARTH_SUN_DISTANCE, 1000000, window);
+  auto EarthAndSun = std::make_shared<Configuration>(
+      "Earth & Sun", 3 * EARTH_SUN_DISTANCE, 1000000, window);
 
   auto height = 2 * EARTH_SUN_DISTANCE;
   auto width = 3 * EARTH_SUN_DISTANCE;
@@ -140,7 +132,7 @@ void CreateEarthAndSun(std::vector<std::shared_ptr<Configuration>> list,
   EarthAndSun->addBody(sun);
   EarthAndSun->addBody(earth);
 
-  list.push_back(std::unique_ptr<Configuration>(EarthAndSun));
+  return EarthAndSun;
 }
 
 void CreateStarCollision(std::vector<std::shared_ptr<Configuration>> list,
@@ -150,8 +142,8 @@ void CreateStarCollision(std::vector<std::shared_ptr<Configuration>> list,
   const float EARTH_MASS = 5.9722E24;
   const float SUN_MASS = 1.98855E30;
 
-  auto StarCollision =
-      new Configuration("Star Collision", 3 * EARTH_SUN_DISTANCE, 1000000, window);
+  auto StarCollision = new Configuration(
+      "Star Collision", 3 * EARTH_SUN_DISTANCE, 1000000, window);
 
   auto height = 3 * EARTH_SUN_DISTANCE;
 
@@ -171,41 +163,41 @@ void CreateStarCollision(std::vector<std::shared_ptr<Configuration>> list,
   list.push_back(std::unique_ptr<Configuration>(StarCollision));
 }
 
-void CreateStableOrbitTwoPlanets(std::vector<std::shared_ptr<Configuration>> list,
-                         sf::RenderWindow& window) {
-auto StableOrbitTwoPlanets =
+void CreateStableOrbitTwoPlanets(
+    std::vector<std::shared_ptr<Configuration>> list,
+    sf::RenderWindow& window) {
+  auto StableOrbitTwoPlanets =
       new Configuration("Two Planets", 1600, 1000000, window);
   std::unique_ptr<Body> p1 =
       std::make_unique<Planet>(Vector{800, 400} * 100, Vector{0, 0}, 10E15);
   std::unique_ptr<Body> p2 =
       std::make_unique<Planet>(Vector{500, 400} * 100, Vector{0, 45}, 10E13);
 
-StableOrbitTwoPlanets->addBody(p1);
-StableOrbitTwoPlanets->addBody(p2);
+  StableOrbitTwoPlanets->addBody(p1);
+  StableOrbitTwoPlanets->addBody(p2);
 
-list.push_back(std::unique_ptr<Configuration>(StableOrbitTwoPlanets));
+  list.push_back(std::unique_ptr<Configuration>(StableOrbitTwoPlanets));
 }
 
-void CreateStableOrbitTwoPlanets2(std::vector<std::shared_ptr<Configuration>> list,
-                         sf::RenderWindow& window) {
-auto StableOrbitTwoPlanets2 =
+void CreateStableOrbitTwoPlanets2(
+    std::vector<std::shared_ptr<Configuration>> list,
+    sf::RenderWindow& window) {
+  auto StableOrbitTwoPlanets2 =
       new Configuration("Two Planets 2", 1600, 1000000, window);
   std::unique_ptr<Body> p1 =
       std::make_unique<Planet>(Vector{800, 400} * 100, Vector{0, 0}, 10E16);
   std::unique_ptr<Body> p2 =
-      std::make_unique<Planet>(Vector{800, 100} * 100, Vector{-180, 0},
-      10E11);
+      std::make_unique<Planet>(Vector{800, 100} * 100, Vector{-180, 0}, 10E11);
 
-StableOrbitTwoPlanets2->addBody(p1);
-StableOrbitTwoPlanets2->addBody(p2);
+  StableOrbitTwoPlanets2->addBody(p1);
+  StableOrbitTwoPlanets2->addBody(p2);
 
-list.push_back(std::unique_ptr<Configuration>(StableOrbitTwoPlanets2));
+  list.push_back(std::unique_ptr<Configuration>(StableOrbitTwoPlanets2));
 }
 
-void CreateThreeBodies(std::vector<std::shared_ptr<Configuration>> list,
-                         sf::RenderWindow& window) {
-auto ThreeBodies =
-      new Configuration("Three Bodies", 2000, 1000000, window);
+auto CreateThreeBodies(sf::RenderWindow& window) {
+  auto ThreeBodies =
+      std::make_shared<Configuration>("Three Bodies", 2000, 1000000, window);
 
   std::unique_ptr<Body> p1 =
       std::make_unique<Planet>(Vector{700, 400}, Vector{0, 30}, 10E14);
@@ -215,32 +207,32 @@ auto ThreeBodies =
   std::unique_ptr<Body> p3 =
       std::make_unique<Planet>(Vector{1200, 500}, Vector{10, 30}, 10E14);
 
-ThreeBodies->addBody(p1);
-ThreeBodies->addBody(p2);
-ThreeBodies->addBody(p3);
+  ThreeBodies->addBody(p1);
+  ThreeBodies->addBody(p2);
+  ThreeBodies->addBody(p3);
 
-list.push_back(std::unique_ptr<Configuration>(ThreeBodies));
+  return ThreeBodies;
 }
 
 void CreateBinaryStars(std::vector<std::shared_ptr<Configuration>> list,
-                         sf::RenderWindow& window) {
-auto BinaryStars =
-      new Configuration("Binary Stars", 2000, 1000000, window);
+                       sf::RenderWindow& window) {
+  auto BinaryStars = new Configuration("Binary Stars", 2000, 1000000, window);
 
   std::unique_ptr<Body> s1 =
       std::make_unique<Planet>(Vector{975, 400}, Vector{0, 25}, 20E14);
   std::unique_ptr<Body> s2 =
       std::make_unique<Planet>(Vector{1100, 400}, Vector{0, -25}, 20E14);
 
-      BinaryStars->addBody(s1);
-      BinaryStars->addBody(s2);
+  BinaryStars->addBody(s1);
+  BinaryStars->addBody(s2);
 
-list.push_back(std::unique_ptr<Configuration>(BinaryStars));
+  list.push_back(std::unique_ptr<Configuration>(BinaryStars));
 }
 
-void CreateCollapsingBinaryStars(std::vector<std::shared_ptr<Configuration>> list,
-                         sf::RenderWindow& window) {
-auto CollapsingBinaryStars =
+void CreateCollapsingBinaryStars(
+    std::vector<std::shared_ptr<Configuration>> list,
+    sf::RenderWindow& window) {
+  auto CollapsingBinaryStars =
       new Configuration("Collapsing Binary Stars", 2000, 1000000, window);
 
   std::unique_ptr<Body> s1 =
@@ -248,10 +240,11 @@ auto CollapsingBinaryStars =
   std::unique_ptr<Body> s2 =
       std::make_unique<Planet>(Vector{1150, 400}, Vector{0, -20}, 50E14);
 
-      CollapsingBinaryStars->addBody(s1);
-      CollapsingBinaryStars->addBody(s2);
+  CollapsingBinaryStars->addBody(s1);
+  CollapsingBinaryStars->addBody(s2);
 
-list.push_back(std::unique_ptr<Configuration>(CollapsingBinaryStars));
+  list.push_back(std::unique_ptr<Configuration>(CollapsingBinaryStars));
 }
 
+};  // namespace gs
 #endif
